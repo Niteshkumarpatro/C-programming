97c4d40c05385675d64f9bc682c90326c65ce437
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-16 23:49:02 +0530

    AREA OF RECTANGLE

---
+++ 04_arearectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+    int length,width,area;
+    length=2;
+    width=8;
+    area=(length*width);
+    printf("%d",area);
+ }
+//  area of rectangle = length*width(breadth)
+// perimeter =2*(length+width)
