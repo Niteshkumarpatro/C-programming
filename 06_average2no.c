ffd58067e9612ef2bb541d1ca9c33c6d9d164b5e
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-17 22:43:54 +0530

    average of 3 number

---
+++ 06_average2no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+ int  main()
+{
+    int a,b,c,sum,average;
+    a=2;
+    b=4;
+    c=6;
+    sum= a+b+c;
+    average=sum/3;
+    printf("%d",sum);
+    printf("%d",average);
+}
