7d8ae7b82e9c65b41041396b1ba38aba505bcb7c
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-18 14:07:09 +0530

    AREA OF TRIANGLE

---
+++ 07_areatriangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+  int base,height;
+ float area;
+  base =2;
+  height =6;
+  area=0.5*(base*height);
+  printf("The area of triangle is %f",area);
+   }
