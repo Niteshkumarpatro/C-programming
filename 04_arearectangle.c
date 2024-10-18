91d30f4185a806946f0c2a45c8bbab8bca6027d7
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-18 13:57:08 +0530

    AREA OF RECTANGLE

--- 04_arearectangle.c
+++ 04_arearectangle.c
@@ -6,7 +6,7 @@
     length=2;
     width=8;
     area=(length*width);
-    printf("%d",area);
+    printf("The area of rectangle is %d",area);
  }
 //  area of rectangle = length*width(breadth)
 // perimeter =2*(length+width)
