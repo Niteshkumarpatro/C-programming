8666fbe636acadbfba18f755d2784aed7d3ac08e
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-18 13:56:57 +0530

    MULTIPLICATION OF 2 NUMBERS

--- 03_mul2no.c
+++ 03_mul2no.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<conio.h>
  int main()
- { int a,b,c;
- a=8;
- b=4;
- c=a*b;
- printf("%d",c);
+ { 
+    int a,b,c;
+  a=8;
+  b=4;
+  c=a*b;
+ printf("the multiplication of two number is %d",c);
 
 
  }
